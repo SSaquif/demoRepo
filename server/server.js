42b7fb020190062ed197c86c3701755ca770e393
Sadnan Saquif <ssaquif@gmail.com>
2020-08-19 11:42:40 -0400

    Server works and also moved stuff around

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+// const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+const PORT = 4000;
+
+express()
+  .use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
+  .use(helmet())
+  .use(morgan("dev"))
+  .use(express.static("./server/assets"))
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use("/", express.static(__dirname + "/"))
+
+  // REST endpoints?
+  .get("/bacon", (req, res) => res.status(200).json("🥓"))
+
+  .listen(PORT, () => console.info(`Listening on port ${PORT}`));
